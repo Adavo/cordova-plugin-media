61785abf0f6b1d5fefdbbecf903198646a62cfa6
Adavo <Adavo@users.noreply.github.com>
2017-05-10 13:34:08 +0800

    Update CDVSound.m
    fix problem sound go down to 60% after recording

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -665,7 +665,7 @@
             // get the audioSession and set the category to allow recording when device is locked or ring/silent switch engaged
             if ([weakSelf hasAudioSession]) {
                 if (![weakSelf.avSession.category isEqualToString:AVAudioSessionCategoryPlayAndRecord]) {
-                    [weakSelf.avSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionMixWithOthers error:nil];
+                    [weakSelf.avSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
                 }
 
                 if (![weakSelf.avSession setActive:YES error:&error]) {
